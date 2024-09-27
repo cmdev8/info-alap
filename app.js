19e141f919de5bf5714939eacb2e91661bfa4f25
Czar Milan <info@ux-studio.com>
2024-09-27 20:26:06 +0200

    update

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ document.addEventListener("alpine:init", () => {
   Alpine.data("app", () => ({
     currentPage: "/",
     messages: [
-      { name: "n1", message: "m1" },
-      { name: "n2", message: "m2" },
+      {
+        name: "Leslie Alexander",
+        message:
+          "Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. ",
+      },
+      {
+        name: "Michael Foster",
+        message:
+          "Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et ",
+      },
     ],
     formName: "",
     formMessage: "",
     formResultMessage: "",
+
+    loginEmail: "",
+    loginPassword: "",
+    loginError: "",
     adminLoggedIn: false,
 
     visit(page) {
@@ -18,14 +30,34 @@ document.addEventListener("alpine:init", () => {
       return this.currentPage === page;
     },
 
+    login() {
+      if (this.loginEmail === "admin@example.com" && this.loginPassword === "123456") {
+        this.adminLoggedIn = true;
+        this.visit("/admin");
+      } else {
+        this.loginError = "Hibás adatok";
+      }
+    },
+
+    logout() {
+      this.loginError = "";
+      this.loginEmail = "";
+      this.loginPassword = "";
+      this.adminLoggedIn = false;
+      this.visit("/");
+    },
+
     sendMessage() {
+      if (this.formName == "" || this.formMessage == "") {
+        this.formResultMessage = "A nevet és az üzenetet kötelező kitölteni!";
+        return;
+      }
       this.messages.push({ name: this.formName, message: this.formMessage });
 
       this.formName = "";
       this.formMessage = "";
 
-      this.formResultMessage =
-        "Koszonjuk az uzenetet, ugysem nezzuk meg es nem is valaszolunk";
+      this.formResultMessage = "Köszönjük az üzenetet!";
     },
   }));
 });
